edefdd0fca394132d07bbc6e929521b45414b480
fidanyucel <fidan.colak52@gmail.com>
2020-09-09 18:11:08 +0300

    Updates

---
+++ precache-manifest.265347169c96703464eb33346659dc22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f9cc87bf9d50e1e96def81b03ad400",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "a5832d626f0a32d7b91f",
+    "url": "/react-deploy/static/css/main.5e10f656.chunk.css"
+  },
+  {
+    "revision": "b36402cde46e6734176d",
+    "url": "/react-deploy/static/js/2.7f50ca75.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-deploy/static/js/2.7f50ca75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5832d626f0a32d7b91f",
+    "url": "/react-deploy/static/js/main.e0a9acf7.chunk.js"
+  },
+  {
+    "revision": "9a6b4006b58ad85b50f6",
+    "url": "/react-deploy/static/js/runtime-main.3e11c617.js"
+  }
+]);
